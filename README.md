ef66fe4d624ec97626867c066d0e5f8203ce3dd0
Acelya-9028 <github@shdw.xyz>
2025-04-14 09:50:55 +0200

    Refactor command-line to combine arguments and update README

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # ratio-spoof
 Ratio-spoof is a cross-platform, free and open source tool to spoof the download/upload amount on private bittorrent trackers.
 
-![](./assets/demo.gif)
+![](./assets/screenshot.png)
+
+## Original author:
+
+**Paulo Afonso Pinheiro:** https://github.com/ap-pauloafonso/ratio-spoof  
 
 ## Motivation
-Here in Brazil, not everybody has a great upload speed, and most private trackers require a ratio greater than or equal to 1. For example, if you downloaded 1GB, you must also upload 1GB in order to survive. Additionally, I have always been fascinated by the BitTorrent protocol. In fact, [I even made a BitTorrent web client to learn more about it](https://github.com/ap-pauloafonso/rwTorrent). So, if you have a bad internet connection, feel free to use this tool. Otherwise, please consider seeding the files with a real torrent client.
+Most private trackers require a ratio greater than or equal to 1. For example, if you downloaded 1GB, you must also upload 1GB in order to survive. So, if you have a bad internet connection, feel free to use this tool. Otherwise, please consider seeding the files with a real torrent client.
 
 ## How does it work?
-![Diagram](./assets/how-it-works.png)
 Bittorrent protocol works in such a way that there is no way that a tracker knows how much certain peer have downloaded or uploaded, so the tracker depends on the peer itself telling the amounts.
 
 Ratio-spoof acts like a normal bittorrent client but without downloading or uploading anything, in fact it just tricks the tracker pretending that.
@@ -15,36 +18,45 @@ Ratio-spoof acts like a normal bittorrent client but without downloading or uplo
 ## Usage
 ```
 usage: 
-	./ratio-spoof -t <TORRENT_PATH> -d <INITIAL_DOWNLOADED> -ds <DOWNLOAD_SPEED> -u <INITIAL_UPLOADED> -us <UPLOAD_SPEED> 
+	./ratio-spoof -t <TORRENT_PATH> -d <INITIAL_DOWNLOADED>:<DOWNLOAD_SPEED> -u <INITIAL_UPLOADED>:<UPLOAD_SPEED> 
 
 optional arguments:
-	-h           		show this help message and exit
-	-p [PORT]    		change the port number, default: 8999
-	-c [CLIENT_CODE]	change the client emulation, default: qbit-4.0.3
+	-h			show this help message and exit
+	-p [PORT]		change the port number, default: 8999
+	-c [CLIENT_CODE]	the client emulation, default: qbit-5.0.4
+	-wait-leechers		wait for leechers instead of uploading with normal speed
 	  
 required arguments:
 	-t  <TORRENT_PATH>     
-	-d  <INITIAL_DOWNLOADED> 
-	-ds <DOWNLOAD_SPEED>						  
-	-u  <INITIAL_UPLOADED> 
-	-us <UPLOAD_SPEED> 						  
+	-d  <INITIAL_DOWNLOADED>:<DOWNLOAD_SPEED> 
+	-u  <INITIAL_UPLOADED>:<UPLOAD_SPEED> 
 	  
-<INITIAL_DOWNLOADED> and <INITIAL_UPLOADED> must be in %, b, kb, mb, gb, tb
-<DOWNLOAD_SPEED> and <UPLOAD_SPEED> must be in kbps, mbps
-[CLIENT_CODE] options: qbit-4.0.3, qbit-4.3.3
+<INITIAL_DOWNLOADED> and <INITIAL_UPLOADED> must be in %
+<DOWNLOAD_SPEED> and <UPLOAD_SPEED> must be in kbps or mbps
+[CLIENT_CODE] options: qbit-4.0.3, qbit-4.3.3, qbit-4.3.9, qbit-4.6.3, qbit-4.6.5, qbit-5.0.0, qbit-5.0.4
 ```
 
-```
-./ratio-spoof -d 90% -ds 100kbps -u 0% -us 1024kbps -t (torrentfile_path) 
-```
-* Will start "downloading" with the initial value of 90% of the torrent total size at 100 kbps speed until it reaches 100% mark.
-* Will start "uploading" with the initial value of 0% of the torrent total size at 1024kbps (aka 1mb/s) indefinitely.
+Examples:
 
 ```
-./ratio-spoof -d 2gb -ds 500kbps -u 1gb -us 1024kbps -t (torrentfile_path) 
+./ratio-spoof -t <TORRENT_PATH> -d 90%:500kbps -u 10%:3mbps
 ```
-* Will start "downloading" with the initial value of 2gb downloaded  if possible at 500kbps speed until it reaches 100% mark.
-* Will start "uploading" with the initial value of 1gb uplodead at 1024kbps (aka 1mb/s) indefinitely.
+* Will start "downloading" with the initial value of 90% of the torrent total size at 500 kbps speed until it reaches 100% mark.
+* Will start "uploading" with the initial value of 10% of the torrent total size at 3 mbps speed indefinitely.
+
+```
+./ratio-spoof -t <TORRENT_PATH> -d 30mbps
+```
+* Same as `./ratio-spoof -t <TORRENT_PATH> -d 100%:30mbps -u 0%:0kbps`
+* Will start "downloading" with the initial value of 100% of the torrent total size at 30 mbps speed.
+* Will not upload anything.
+
+```
+./ratio-spoof -t <TORRENT_PATH> -u 2mbps
+```
+* Same as `./ratio-spoof -t <TORRENT_PATH> -d 100%:0kbps -u 0%:2mbps`
+* Will start with 100% downloaded.
+* Will start "uploading" with the initial value of 0% of the torrent total size at 2 mbps speed indefinitely.
 
 ## Building from Source
 
@@ -77,10 +89,8 @@ This will create a zip file in the `out` directory containing binaries for all p
 Depends on whether you use it carefully, It's a hard task to catch cheaters, but if you start uploading crazy amounts out of nowhere or seeding something with no active leecher on the swarm you may be in risk.
 
 ## Bittorrent client supported 
-The default client emulation is qbittorrent v4.0.3, however you can change it by using the -c argument
+The default client emulation is qbittorrent v5.0.4, however you can change it by using the -c argument
 
 ## Resources
-http://www.bittorrent.org/beps/bep_0003.html
-
+http://www.bittorrent.org/beps/bep_0003.html  
 https://wiki.theory.org/BitTorrentSpecification
-
--- input/input_test.go
+++ input/input_test.go
@@ -19,8 +19,8 @@ func CheckError(out error, want error, t *testing.T) {
 	if out != nil && want != nil && out.Error() != want.Error() {
 		t.Errorf("got %v, want %v", out.Error(), want.Error())
 	}
-
 }
+
 func TestExtractInputInitialByteCount(T *testing.T) {
 	data := []struct {
 		name            string
@@ -30,43 +30,58 @@ func TestExtractInputInitialByteCount(T *testing.T) {
 		err             error
 	}{
 		{
-			name:            "[Donwloaded  - error if higher]100kb input with 200kb limit shouldn't return error test",
-			inSize:          "100kb",
+			name:            "[Downloaded - error if higher] 50% input with 200kb limit shouldn't return error test",
+			inSize:          "50%",
 			inTotal:         204800,
 			inErrorIfHigher: true,
 		},
 		{
-			name:            "[Donwloaded -  error if higher]300kb input with 200kb limit should return error test",
-			inSize:          "300kb",
+			name:            "[Downloaded - error if higher] 150% input with 200kb limit should return error test",
+			inSize:          "150%",
 			inTotal:         204800,
 			inErrorIfHigher: true,
-			err:             errors.New("initial downloaded can not be higher than the torrent size"),
+			err:             errors.New("percentage must be between 0 and 100"),
 		},
 		{
-			name:            "[Uploaded]100kb input with 200kb limit shouldn't return error test",
-			inSize:          "100kb",
+			name:            "[Uploaded] 50% input with 200kb limit shouldn't return error test",
+			inSize:          "50%",
 			inTotal:         204800,
 			inErrorIfHigher: false,
 		},
 		{
-			name:            "[Uploaded]300kb input with 200kb limit shouldn't return error test",
-			inSize:          "300kb",
+			name:            "[Uploaded] 150% input with 200kb limit shouldn't return error test",
+			inSize:          "150%",
 			inTotal:         204800,
 			inErrorIfHigher: false,
+			err:             errors.New("percentage must be between 0 and 100"),
 		},
 		{
-			name:            "[Donwloaded] -100kb should return negative number error test",
-			inSize:          "-100kb",
+			name:            "[Downloaded] -50% should return negative number error test",
+			inSize:          "-50%",
 			inTotal:         204800,
 			inErrorIfHigher: true,
-			err:             errors.New("initial value can not be negative"),
+			err:             errors.New("percentage must be between 0 and 100"),
 		},
 		{
-			name:            "[Uploaded] -100kb should return negative number error test",
-			inSize:          "-100kb",
+			name:            "[Uploaded] -50% should return negative number error test",
+			inSize:          "-50%",
 			inTotal:         204800,
 			inErrorIfHigher: false,
-			err:             errors.New("initial value can not be negative"),
+			err:             errors.New("percentage must be between 0 and 100"),
+		},
+		{
+			name:            "Invalid format should return error test",
+			inSize:          "50kb",
+			inTotal:         204800,
+			inErrorIfHigher: true,
+			err:             errors.New("initial value must be in percentage"),
+		},
+		{
+			name:            "Invalid percentage value should return error test",
+			inSize:          "abc%",
+			inTotal:         204800,
+			inErrorIfHigher: true,
+			err:             errors.New("invalid percentage value"),
 		},
 	}
 
@@ -77,114 +92,8 @@ func TestExtractInputInitialByteCount(T *testing.T) {
 		})
 	}
 }
-func TestStrSize2ByteSize(T *testing.T) {
-
-	data := []struct {
-		name        string
-		in          string
-		inTotalSize int
-		out         int
-		err         error
-	}{
-		{
-			name:        "100kb test",
-			in:          "100kb",
-			inTotalSize: 100,
-			out:         102400,
-		},
-		{
-			name:        "1kb test",
-			in:          "1kb",
-			inTotalSize: 0,
-			out:         1024,
-		},
-		{
-			name:        "1mb test",
-			in:          "1mb",
-			inTotalSize: 0,
-			out:         1048576,
-		},
-		{
-			name:        "1gb test",
-			in:          "1gb",
-			inTotalSize: 0,
-			out:         1073741824,
-		},
-		{
-			name:        "1.5gb test",
-			in:          "1.5gb",
-			inTotalSize: 0,
-			out:         1610612736,
-		},
-		{
-			name:        "1tb test",
-			in:          "1tb",
-			inTotalSize: 0,
-			out:         1099511627776,
-		},
-		{
-			name:        "1b test",
-			in:          "1b",
-			inTotalSize: 0,
-			out:         1,
-		},
-		{
-			name:        "10xb test",
-			in:          "10xb",
-			inTotalSize: 0,
-			err:         errors.New("invalid input size"),
-		},
-		{
-			name:        `100% test`,
-			in:          "100%",
-			inTotalSize: 10737418240,
-			out:         10737418240,
-		},
-		{
-			name:        `55% test`,
-			in:          "55%",
-			inTotalSize: 943718400,
-			out:         519045120,
-		},
-		{
-			name: `5kg test`,
-			in:   "5kg",
-			err:  errors.New("Size not found"),
-		},
-		{
-			name: `-1% test`,
-			in:   "-1%",
-			err:  errors.New("percent value must be in (0-100)"),
-		},
-		{
-			name: `101% test`,
-			in:   "101%",
-			err:  errors.New("percent value must be in (0-100)"),
-		},
-		{
-			name: `a% test`,
-			in:   "a%",
-			err:  errors.New("percent value must be in (0-100)"),
-		},
-	}
-
-	for _, td := range data {
-		T.Run(td.name, func(t *testing.T) {
-			got, err := strSize2ByteSize(td.in, td.inTotalSize)
-			if td.err != nil {
-				if td.err.Error() != err.Error() {
-					t.Errorf("got %v, want %v", err.Error(), td.err.Error())
-				}
-			}
-			if got != td.out {
-				t.Errorf("got %v, want %v", got, td.out)
-			}
-		})
-	}
-}
 
 func TestExtractInputByteSpeed(T *testing.T) {
-
 	data := []struct {
 		name     string
 		speed    string
@@ -205,11 +114,6 @@ func TestExtractInputByteSpeed(T *testing.T) {
 			name:     "1mbps test",
 			speed:    "1mbps",
 			expected: 1048576,
-		},
-		{
-			name:     "2.5mbps test",
-			speed:    "2.5mbps",
-			expected: 2621440,
 		},
 		{
 			name:  "2.5tbps test",
